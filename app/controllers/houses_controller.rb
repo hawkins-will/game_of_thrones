474322501838e0ffe89292f41b6d789352edd3a1
Will Hawkins <willhawkins123@yahoo.com>
2017-02-24 12:54:47 -0500

    Edit and Update for Houses

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -25,12 +25,15 @@ class HousesController < ApplicationController
 
 
   def edit
-
+    @house = House.find(params[:id])
   end
 
 
   def update
+    @house = House.find(params[:id])
+    @house.update(house_params)
 
+    redirect_to "/houses/#{@house.id}"
   end
 
 
